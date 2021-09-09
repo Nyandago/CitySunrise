b2d71ea4150f8c0abdc40360a3a79c43eb6b19be
nyandago <paulmutator@gmail.com>
2021-09-09 23:07:48 +0300

    App Layout Display

--- app/src/main/java/com/cannybits/citysunrise/MainActivity.kt
+++ app/src/main/java/com/cannybits/citysunrise/MainActivity.kt
@@ -2,10 +2,16 @@ package com.cannybits.citysunrise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+  fun GetSunSet(view:View){
+        var city = "sunnyvale,ca"
+        val url = "https://weather-ydn-yql.media.yahoo.com/forecastrss?location="+city+"&format=json"
     }
 }
